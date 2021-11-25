39d12592d79e02c69bad4b1e24bfe81646a3b3f1
DENISA HOSU <denisa.hosu@stud.ubbcluj.ro>
2021-11-26 00:11:14 +0200

    Add project files.

---
+++ Data/Hosu_Denisa_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hosu_Denisa_Lab8.Models;
+
+namespace Hosu_Denisa_Lab8.Data
+{
+    public class Hosu_Denisa_Lab8Context : DbContext
+    {
+        public Hosu_Denisa_Lab8Context (DbContextOptions<Hosu_Denisa_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hosu_Denisa_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Hosu_Denisa_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Hosu_Denisa_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Hosu_Denisa_Lab8.Data;
+using Hosu_Denisa_Lab8.Models;
+
+namespace Hosu_Denisa_Lab8.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Hosu_Denisa_Lab8.Data.Hosu_Denisa_Lab8Context _context;
+
+        public CreateModel(Hosu_Denisa_Lab8.Data.Hosu_Denisa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
